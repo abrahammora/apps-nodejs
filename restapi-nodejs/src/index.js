a654549ad57d0a799e436c991ae47c58e41b9347
Abraham Mora <abraham.mora@smartkode.mx>
2020-11-18 16:57:44 -0600

    created server and home route

---
+++ restapi-nodejs/src/index.js
@@ -0,0 +1,24 @@
+/*Required express to create the server*/
+const express = require('express');
+const app = express();
+/*Require morgan (middleware)*/
+const morgan = require('morgan');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+
+// middlewares
+app.use(morgan('dev'));
+//data from input form
+app.use(express.urlencoded({extended: false}));
+//content json
+app.use(express.json());
+
+//routes
+app.use(require('./routes/index.js'));
+app.use('/api/movies',require('./routes/movies.js'));
+// starting the server
+app.listen(app.get('port'),() => {
+    console.log(`Server on port ${3000}`);
+});
